db2c9dee6129de2bd3eb1c6fb8881a1506738bb0
Praveen Singh <singhpravu@gmail.com>
2025-02-19 00:21:19 +0530

    Update AboutMe component with new developer name; enhance Skills component to use Next.js Image component for better performance

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Slills() {
   const ImagesData = [
     {
@@ -10,25 +12,32 @@ export default function Slills() {
         "https://cdn.jsdelivr.net/npm/devicon@2.16.0/icons/html5/html5-original.svg",
       name: "HTML",
     },
-   
   ];
-    return (
+  return (
+    <div>
+      <h3>Skills</h3>
       <div>
-        <h3>Skills</h3>
+        <h4>Front-end</h4>
         <div>
-          <h4>Front-end</h4>
-          <div>
-            {ImagesData?.map((data, index) => {
-              return (
-                <div key={index}>
-                  <img src={data.imageUrl} alt={data.name} className=" w-[40px] h-[40px] " />
-                  <p>{data.name}</p>
+          {ImagesData?.map((data, index) => {
+            return (
+              <div key={index}>
+                <div className="w-[40px] h-[40px] relative">
+                  <Image
+                    src={data.imageUrl}
+                    alt={data.name}
+                    className=" object-fill"
+                    width={500}
+                    height={500}
+                  />
                 </div>
-              );
-            })}
-           
-          </div>
+
+                <p>{data.name}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
-    );
+    </div>
+  );
 }
